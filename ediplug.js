f60a8d5e2272896f5c6e20da165e81d006ae357b
Lowry Tang <yun-long.tang@hp.com>
2017-08-14 08:50:26 -0700

    update addr

--- ediplug.js
+++ ediplug.js
@@ -2,15 +2,32 @@ const smartplug = require('edimax-smartplug')
 const options = {
   timeout: 10000,
   name: 'edimax',
-  host: '192.168.1.14',
+  host: '192.168.1.13',
   username: 'admin',
   password: '880711'
 }
-// smartplug.getDeviceInfo(options).then(function (info) {
-//   console.log(info)
-// }).catch(function (e) {
-//   console.log('Request failed: ', e)
-// })
+
+// discover devices
+// smartplug
+//   .discoverDevices({
+//     timeout: 3000,
+//     address: '192.168.1.255'
+//   })
+//   .then(function(results) {
+//     console.log('Discovery Result:', results)
+//   })
+//   .catch(function(e) {
+//     console.log('Request failed: ', e)
+//   })
+
+// smartplug
+//   .getDeviceInfo(options)
+//   .then(function(info) {
+//     console.log(info)
+//   })
+//   .catch(function(e) {
+//     console.log('Request failed: ', e)
+//   })
 
 module.exports = {
   switchLamp (status = false) {
--- index.js
+++ index.js
@@ -3,22 +3,6 @@ const app = express()
 
 const ediplug = require('./ediplug')
 
-app.get('/home/open', function (req, res) {
-  ediplug.switchLamp(true).then(() => {
-    res.send('open')
-  }).catch(err => {
-    res.send(err)
-  })
-})
-
-app.get('/home/close', function (req, res) {
-  ediplug.switchLamp(false).then(() => {
-    res.send('close')
-  }).catch(err => {
-    res.send(err)
-  })
-})
-
 app.get('/home/switch', function (req, res) {
   ediplug.getState().then(state => {
     return ediplug.switchLamp(!state).then(() => {
